c870b3f4b31aab9b193ae4d24ffe63fd71673e3a
sofiagakalo1 <108238510+sofiagakalo1@users.noreply.github.com>
2022-11-04 13:32:12 +0100

    task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,33 @@
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+
+const counter = document.querySelector('#value');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+// console.log(counter);
+// console.log(decrementBtn);
+// console.log(incrementBtn);
+
+let counterValue = 0;
+const render = () => {
+  // counter.innerHTML = '';
+  // counter.insertAdjacentHTML('afterbegin', `${counterValue}`);
+  counter.textContent = counterValue;
+};
+
+const onDecrementBtn = () => {
+  counterValue = counterValue - 1;
+  // counterValue -=1;
+  render();
+};
+
+const onIncrementBtn = () => {
+  counterValue += 1;
+  render();
+};
+
+render();
+
+decrementBtn.addEventListener('click', onDecrementBtn);
+incrementBtn.addEventListener('click', onIncrementBtn);
