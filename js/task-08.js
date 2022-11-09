d2b4c03678755589ed2a4c4c846db3b2c35d2820
sofiagakalo1 <108238510+sofiagakalo1@users.noreply.github.com>
2022-11-09 15:03:23 +0100

    task8 - fixed

--- js/task-08.js
+++ js/task-08.js
@@ -30,7 +30,7 @@ const onSubmitBtn = (event) => {
   if (email.value === "" || password.value === "") {
     return alert("Всі поля повинні бути заповнені!");
   }
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  console.log(`{Email: ${email.value}, Password: ${password.value}}`);
   event.currentTarget.reset();
 };
 
